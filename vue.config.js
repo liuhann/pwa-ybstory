9e22d9129d10cf16268c011a512789ef41c73eac
liuhan <liuhann@gmail.com>
2018-07-31 13:44:27 +0800

    12

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,23 @@
 // vue.config.js
 module.exports = {
-	// options...
-	baseUrl: process.env.NODE_ENV === 'production'
-		? './'
-		: './',
+  // options...
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? './'
+    : './',
 
-	pages: {
-		index: {
-			// entry for the page
-			entry: 'src/main.js',
-			// the source template
-			template: 'src/index.html',
-			// output as dist/index.html
-			filename: 'index.html',
-			// when using title option,
-			// template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-			title: 'Index Page'
-		}
-	},
-	transpileDependencies: ['async-boot']
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'src/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Index Page'
+    }
+  },
+  outputDir: './cordova-story-v3/www',
+  transpileDependencies: ['async-boot']
 }
